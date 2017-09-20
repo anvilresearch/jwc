b73fd6200eb1fb96a67ca781fd7bab7b7ee3ce07
Greg Linklater <greglink49@gmail.com>
2017-09-20 12:10:58 +0200

    chore(mocha): mocha options and placeholder test

---
+++ test/JWCSpec.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/**
+ * Test dependencies
+ */
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+/**
+ * Assertions
+ */
+chai.use(chaiAsPromised)
+chai.should()
+let expect = chai.expect
+
+/**
+ * Code under test
+ */
+
+/**
+ * Test Data
+ */
+
+/**
+ * JWC
+ */
+describe('JWC', () => {
+})
